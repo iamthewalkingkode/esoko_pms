19c1744e128360026ae23187915eea1eaf03d661
iamTheWalkingKode <emmanuel@anchoratechs.com>
2019-08-29 11:51:15 +0000

    ...

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const conf = {
-    port: 8000,
+    port: 7000,
     pepper: 'WybHhZwck32eWKbLMcqTUQFk3U9tqjDSEWk7ijG9qcIPT7Ex29MVvhamLVDm6H2p',
     database: 'mongodb://localhost/esoko_pms',
     jwtExpiresIn: '7d'
